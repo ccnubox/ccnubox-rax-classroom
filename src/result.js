7feeb48ac79bff6fd02a65b49b7cbeb7a9b8ff50
1 <1305233756@qq.com>
2018-08-06 21:40:04 +0800

    feat: change styles of result

--- src/result.js
+++ src/result.js
@@ -2,7 +2,9 @@ import { createElement, Component } from 'rax';
 import View from 'rax-view';
 import Text from 'rax-text';
 import styles from './result.css';
-import ListView from 'rax-listview';
+import TabHeader from 'rax-tabheader';
+import Image from 'rax-image';
+import Touchable from 'rax-touchable';
 import MultiRow from 'rax-multirow';
 import InfoService from './services/index.js';
 
@@ -13,6 +15,14 @@ function getQueryString(name) {
   return null;  
 }
 
+const classroomInset = {
+  uri: 'http://ocm66x3nz.bkt.clouddn.com/classroomInset.png'
+};
+const classroomError = {
+  uri: 'http://ocm66x3nz.bkt.clouddn.com/audit_error.png'
+
+};
+
 const Options = {
   weekno: '1',
   weekday: 'mon',
@@ -21,31 +31,31 @@ const Options = {
 
 let ClassRoom = [
   {
-    title: "上午8:00空闲教室",
+    title: "8:00",
     numbers: []
   },
   {
-    title: "上午10:00空闲教室",
+    title: "10:00",
     numbers: []
   },
   {
-    title: "上午12:00空闲教室",
+    title: "12:00",
     numbers: []
   },
   {
-    title: "下午2:00空闲教室",
+    title: "14:00",
     numbers: []
   },
   {
-    title: "上午4:00空闲教室",
+    title: "16:00",
     numbers: []
   },
   {
-    title: "上午6:00空闲教室",
+    title: "18:00",
     numbers: []
   },
   {
-    title: "上午8:00空闲教室",
+    title: "20:00",
     numbers: []
   }
 ]
@@ -54,25 +64,40 @@ class Result extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      ClassRoom: []
+      ClassRoom: [],
+      ClassRoomShow: [],
+      error: false
     };
   }
 
-  componentWillMount() {
+  fetchClassRoom = () => {
     // 获取查询参数
     Options.building = getQueryString("building")
     Options.weekday = getQueryString("weekday")
     Options.weekno = getQueryString("weekno")
     // 发送查询请求
-    InfoService.getClassRoom(Options).then(data => {
+    InfoService.getClassRoom(Options).
+    then(data => {
       ClassRoom.forEach((el, index, array) => {
         let n = index * 2 + 1
         array[index].numbers = data[n]
       })
       this.setState({
-        ClassRoom
+        ClassRoom,
+        ClassRoomShow: ClassRoom[0].numbers,
+        error: false
+      })
+    })
+    .catch(data => {
+      alert(data)
+      this.setState({
+        error: true
       })
     });
+  }
+
+  componentWillMount() {
+    this.fetchClassRoom()
   }
 
   listItem = (item, index) => {
@@ -96,18 +121,91 @@ class Result extends Component {
     );
   }
 
+  renderItem = (item, index) => {
+    return <View style={styles.tabHeaderItem}>
+            <View style={styles.tabHeaderLine}>
+              <View style={styles.timeLine}></View>
+              <View style={styles.tabHeaderDot}></View>
+              <View style={styles.timeLine}></View>
+            </View> 
+            <Text style={styles.tabHeaderText}>{item.title}</Text>
+          </View>;
+  }
+  renderSelect = (item, index) => {
+    return <View style={styles.tabHeaderItem}>
+            <View style={styles.tabHeaderLine}>
+              <View style={styles.timeLine}></View>
+              <View style={styles.tabHeaderDot_select}></View>
+              <View style={styles.timeLine}></View>
+            </View> 
+            <Text style={styles.tabHeaderText_select}>{item.title}</Text>
+          </View>;
+  }
+  onSelect = (index) => {
+    this.setState({
+      ClassRoomShow: this.state.ClassRoom[index].numbers
+    })
+  }
+  itemWidth = (item, index) => {
+    return parseInt(750 / this.state.ClassRoom.length) + 'rem';
+  }
+  
+
   render() {
-    return (
-      <View style={styles.app}>
-        <View style={styles.container}>
-          <ListView
-          renderRow={this.listItem}
-          dataSource={this.state.ClassRoom}
-          />
+    if (this.state.error) {
+      return (
+        <View style={styles.error_app}>
+          <View style={styles.error_container}>
+            <Image style={styles.error_image} source={classroomError} />
+            <View style={styles.error_text}>
+              <Text style={styles.error_text_info}>出错啦，</Text>
+              <Touchable onPress={() => {this.fetchClassRoom()}}>
+                <Text style={styles.error_text_click}>点击刷新</Text>
+              </Touchable>
+            </View>
+            
+          </View>
         </View>
-      </View>
-      
-    );
+      )
+    }
+    else {
+      return (
+        <View style={styles.app}>
+          <View style={styles.tabHeaderSpace}>
+          </View>
+          <TabHeader 
+            style={styles.tabHeader} 
+            dataSource={this.state.ClassRoom} 
+            renderItem={this.renderItem} 
+            renderSelect={this.renderSelect} 
+            onSelect={this.onSelect}
+            selected={0}
+            itemWidth={this.itemWidth}
+          />
+          <View style={styles.insetImg}>
+            <Image
+                style={styles.classroomInset}
+                source={classroomInset}
+              />
+          </View>
+          <View style={styles.item}>
+            <View style={styles.numbers_container}>
+              <MultiRow
+                  dataSource={this.state.ClassRoomShow}
+                  cells={5}
+                  renderCell={(item, index) => {
+                    return (
+                      <View style={styles.numbers_item}>
+                        <Text style={styles.numbers_text}>{item}</Text>
+                      </View>
+                    );
+                  }
+                } />
+            </View>
+          </View>
+        </View>
+      );
+    }
   }
 }
 
