0533b1a2e84698cf050b8f5d440e56ad99bcc371
zxc0328 <120797887@qq.com>
2018-10-24 15:07:33 +0800

    feat: using forked picker for ux

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import View from "rax-view";
 import Text from "rax-text";
 import Image from "rax-image";
 import styles from "./App.css";
-import Picker from "rax-picker";
+import Picker from "./picker";
 import Touchable from "rax-touchable";
 const native = require("@weex-module/test");
 import Option from "./optionMap";
-import arrowIcon from "./assets/arrow.png"
+import arrowIcon from "./assets/arrow.png";
 
 class App extends Component {
   constructor(props) {
@@ -28,64 +28,85 @@ class App extends Component {
           <View style={styles.select_item}>
             <Text style={styles.select_title}>周数：</Text>
             <View style={styles.select_bar}>
-              <View style={styles.select_bar_content}>
-                <Picker
-                  selectedValue={this.state.weekno}
-                  onValueChange={item => {
-                    this.setState({
-                      weekno: item
-                    });
-                  }}
-                  style={styles.select_bar_text}
-                >
-                  {Option.weeknoMap.map(item => (
-                    <Picker.Item value={item.value} label={item.label} />
-                  ))}
-                </Picker>
-                <Image style={styles.arrow_icon} source={arrowIcon} />
-              </View>
+              <Picker
+                renderLabel={(textStyle, selectedLabel) => {
+                  return (
+                    <View style={styles.select_bar_content}>
+                      <View style={styles.select_bar_content_text}>
+                        <Text style={textStyle}>{selectedLabel}</Text>
+                      </View>
+                      <Image style={styles.arrow_icon} source={arrowIcon} />
+                    </View>
+                  );
+                }}
+                selectedValue={this.state.weekno}
+                onValueChange={item => {
+                  this.setState({
+                    weekno: item
+                  });
+                }}
+                style={styles.select_bar_text}
+              >
+                {Option.weeknoMap.map(item => (
+                  <Picker.Item value={item.value} label={item.label} />
+                ))}
+              </Picker>
             </View>
           </View>
           <View style={styles.select_item}>
             <Text style={styles.select_title}>星期：</Text>
             <View style={styles.select_bar}>
-              <View style={styles.select_bar_content}>
-                <Picker
-                  selectedValue={this.state.weekday}
-                  onValueChange={item => {
-                    this.setState({
-                      weekday: item
-                    });
-                  }}
-                  style={styles.select_bar_text}
-                >
-                  {Option.weekdayMap.map(item => (
-                    <Picker.Item value={item.value} label={item.label} />
-                  ))}
-                </Picker>
-                <Image style={styles.arrow_icon} source={arrowIcon} />
-              </View>
+              <Picker
+                renderLabel={(textStyle, selectedLabel) => {
+                  return (
+                    <View style={styles.select_bar_content}>
+                      <View style={styles.select_bar_content_text}>
+                        <Text style={textStyle}>{selectedLabel}</Text>
+                      </View>
+                      <Image style={styles.arrow_icon} source={arrowIcon} />
+                    </View>
+                  );
+                }}
+                selectedValue={this.state.weekday}
+                onValueChange={item => {
+                  this.setState({
+                    weekday: item
+                  });
+                }}
+                style={styles.select_bar_text}
+              >
+                {Option.weekdayMap.map(item => (
+                  <Picker.Item value={item.value} label={item.label} />
+                ))}
+              </Picker>
             </View>
           </View>
           <View style={styles.select_item}>
             <Text style={styles.select_title}>自习地点：</Text>
             <View style={styles.select_bar}>
-              <View style={styles.select_bar_content}>
-                <Picker
-                  selectedValue={this.state.building}
-                  onValueChange={item => {
-                    this.setState({
-                      building: item
-                    });
-                  }}
-                  style={styles.select_bar_text}
-                >
-                  {Option.buildingMap.map(item => (
-                    <Picker.Item value={item.value} label={item.label} />
-                  ))}
-                </Picker>
-                <Image style={styles.arrow_icon} source={arrowIcon} />
-              </View>
+              <Picker
+                renderLabel={(textStyle, selectedLabel) => {
+                  return (
+                    <View style={styles.select_bar_content}>
+                      <View style={styles.select_bar_content_text}>
+                        <Text style={textStyle}>{selectedLabel}</Text>
+                      </View>
+                      <Image style={styles.arrow_icon} source={arrowIcon} />
+                    </View>
+                  );
+                }}
+                selectedValue={this.state.building}
+                onValueChange={item => {
+                  this.setState({
+                    building: item
+                  });
+                }}
+                style={styles.select_bar_text}
+              >
+                {Option.buildingMap.map(item => (
+                  <Picker.Item value={item.value} label={item.label} />
+                ))}
+              </Picker>
             </View>
           </View>
           <Touchable
